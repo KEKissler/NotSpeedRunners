ffad9090721cc65994eb0a58952b9e943e75c5bb
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-04-09 15:34:40 -0700

    Add 2d camera

--- Assets/Scripts/BasicCameraController.cs
+++ Assets/Scripts/BasicCameraController.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class BasicCameraController : MonoBehaviour {
     //x dir is all
     public GameObject player;
-    public float lerpAmount;
+    public float baseLerpAmount;
+    public float lerpAmpBySpeed;
     public float velocityPredictAmount;
 
-    private float fixedYPos;
     private float fixedZPos;
     private GrappleMovementController gmc;
     private Rigidbody rb;
+    private Vector2 veloLastFrame = new Vector2(), veloThisFrame = new Vector2();
 	// Use this for initialization
 	void Start () {
         fixedZPos = transform.position.z;
-        fixedYPos = transform.position.y;
         rb = player.GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.LerpUnclamped(transform.position, new Vector3(player.transform.position[0] + rb.velocity[0] * velocityPredictAmount, fixedYPos, fixedZPos), lerpAmount * Time.deltaTime);
+        float lerpAmount = baseLerpAmount + lerpAmpBySpeed * Vector2.Distance((Vector2)(rb.position), (Vector2)(transform.position));
+        Vector3 desiredNextCamPos = 
+            Vector3.LerpUnclamped(
+                new Vector3(rb.position.x, rb.position.y, fixedZPos), 
+                new Vector3(rb.position[0] + rb.velocity[0] * velocityPredictAmount,
+                            rb.position[1] + rb.velocity[1] * velocityPredictAmount,
+                            fixedZPos),
+                lerpAmount * Time.deltaTime);
+
+        transform.position = Vector3.LerpUnclamped(transform.position, desiredNextCamPos, 0.1f);
 	}
 }
