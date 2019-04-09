dc4861950d5358ce2c0b1140f8236ddeabcc92f1
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-04-09 15:50:19 -0700

    readd smooth interp movement

--- Assets/Scripts/BasicCameraController.cs
+++ Assets/Scripts/BasicCameraController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,4 +23,41 @@ public class BasicCameraController : MonoBehaviour {
 	void Update () {
         transform.position = Vector3.LerpUnclamped(transform.position, new Vector3(player.transform.position[0] + rb.velocity[0] * velocityPredictAmount, fixedYPos, fixedZPos), lerpAmount * Time.deltaTime);
 	}
+}*/
+
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCameraController : MonoBehaviour {
+    //x dir is all
+    public GameObject player;
+    public float baseLerpAmount;
+    public float lerpAmpBySpeed;
+    public float velocityPredictAmount;
+
+    private float fixedZPos;
+    private GrappleMovementController gmc;
+    private Rigidbody rb;
+    private Vector2 veloLastFrame = new Vector2(), veloThisFrame = new Vector2();
+	// Use this for initialization
+	void Start () {
+        fixedZPos = transform.position.z;
+        rb = player.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float lerpAmount = baseLerpAmount + lerpAmpBySpeed * Vector2.Distance((Vector2)(rb.position), (Vector2)(transform.position));
+        Vector3 desiredNextCamPos = 
+            Vector3.LerpUnclamped(
+                new Vector3(rb.position.x, rb.position.y, fixedZPos), 
+                new Vector3(rb.position[0] + rb.velocity[0] * velocityPredictAmount,
+                            rb.position[1] + rb.velocity[1] * velocityPredictAmount,
+                            fixedZPos),
+                lerpAmount * Time.deltaTime);
+
+        transform.position = Vector3.LerpUnclamped(transform.position, desiredNextCamPos, 0.1f);
+	}
 }
+
