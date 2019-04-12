3d34473e1e3e12f6d25cdcc23c493b9c788f899a
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-04-12 13:16:36 -0700

    Add level 2

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public static LevelLoader instance;
+
+	void Awake () {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; ++i)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                TargetController.RemainingTargets = 0;
+                RespawnPointController test = GameObject.Find("RespawnPoint").GetComponent<RespawnPointController>();
+                test.teleportPlayerToLastRespawnPoint();
+                Debug.Log("1");
+                SceneManager.LoadScene(i, LoadSceneMode.Single);
+                break;
+            }
+        }
+    }
+}
