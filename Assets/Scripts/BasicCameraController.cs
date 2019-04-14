f811d5fa0c564c3e60d7459af238964f127e1d61
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-04-13 17:20:48 -0700

    Finalize Level 2

--- Assets/Scripts/BasicCameraController.cs
+++ Assets/Scripts/BasicCameraController.cs
@@ -35,24 +35,28 @@ public class BasicCameraController : MonoBehaviour {
     public float baseLerpAmount;
     public float lerpAmpBySpeed;
     public float velocityPredictAmount;
+    public bool freezeYPos;
+
 
     private float fixedZPos;
+    private float fixedYPos;
     private GrappleMovementController gmc;
     private Rigidbody rb;
-    private Vector2 veloLastFrame = new Vector2(), veloThisFrame = new Vector2();
 	// Use this for initialization
 	void Start () {
         fixedZPos = transform.position.z;
+        fixedYPos = transform.position.y;
         rb = player.GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         float lerpAmount = baseLerpAmount + lerpAmpBySpeed * Vector2.Distance((Vector2)(rb.position), (Vector2)(transform.position));
-        Vector3 desiredNextCamPos = 
+        Vector3 desiredNextCamPos =
             Vector3.LerpUnclamped(
-                new Vector3(rb.position.x, rb.position.y, fixedZPos), 
+                new Vector3(rb.position.x, freezeYPos ? fixedYPos : rb.position.y, fixedZPos),
                 new Vector3(rb.position[0] + rb.velocity[0] * velocityPredictAmount,
+                            freezeYPos ? fixedYPos :
                             rb.position[1] + rb.velocity[1] * velocityPredictAmount,
                             fixedZPos),
                 lerpAmount * Time.deltaTime);
